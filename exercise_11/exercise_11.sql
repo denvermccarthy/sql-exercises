5de07262c4e059833783a8f29834b0ce5ba36dc0
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-15 15:46:20 -0700

    added excercises 10 - 15

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,9 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+select title, count(rental.inventory_id) from film
+left join inventory on film.film_id = inventory.film_id
+left join rental on inventory.inventory_id = rental.inventory_id
+group by film.film_id
+order by count(rental.inventory_id) desc
+limit 10
